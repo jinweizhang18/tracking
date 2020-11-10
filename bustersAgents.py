0349e5eac6b2209199fd5c0546df28ac4973cdf2
Jinwei Zhang <jinweizhang18@gmail.com>
2020-11-09 23:33:40 -0800

    p4

--- bustersAgents.py
+++ bustersAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -143,4 +143,12 @@ class GreedyBustersAgent(BustersAgent):
         livingGhostPositionDistributions = \
             [beliefs for i, beliefs in enumerate(self.ghostBeliefs)
              if livingGhosts[i+1]]
-        "*** YOUR CODE HERE ***"
+        likely_pos_ghosts = [x.argMax() for x in livingGhostPositionDistributions]
+        distances = [(self.distancer.getDistance(pacmanPosition, pos), pos) for pos in likely_pos_ghosts]
+        nearestGhostpos = min(distances, key = lambda t: t[0])[1]
+        actions = []
+        for action in legal:
+            successorPosition = Actions.getSuccessor(pacmanPosition, action)
+            dist = self.distancer.getDistance(successorPosition, nearestGhostpos)
+            actions.append((action, dist))
+        return min(actions, key = lambda t: t[1])[0]
